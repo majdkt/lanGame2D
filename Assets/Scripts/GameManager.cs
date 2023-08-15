4af6e4c11c4390542a73e3f492f225f4512fdd28
majd_kt <majd.kt97@gmail.com>
2023-08-16 01:58:39 +0200

    update tested and all fine

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -95,9 +95,7 @@ public class GameManager : MonoBehaviour
         while (n > 1)
         {
             int k = Random.Range(0, n--);
-            T temp = array[n];
-            array[n] = array[k];
-            array[k] = temp;
+            (array[n], array[k]) = (array[k], array[n]);
         }
     }
 
@@ -137,6 +135,7 @@ public class GameManager : MonoBehaviour
         UpdateScoreText();
     }
 
+    // ReSharper disable Unity.PerformanceAnalysis
     private void RestartGame()
     {
         AssignGermanWordToText();
